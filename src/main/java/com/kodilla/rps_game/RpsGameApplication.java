b4381f899527782dd86872e7648882b7137b1131
rydzwr <rydzwr@gmail.com>
2021-11-24 03:37:40 +0100

    Preparing...

--- src/main/java/com/kodilla/rps_game/RpsGameApplication.java
+++ src/main/java/com/kodilla/rps_game/RpsGameApplication.java
@@ -1,9 +1,16 @@
 package com.kodilla.rps_game;
 
+import java.util.Scanner;
+
 public class RpsGameApplication
 {
     public static void main(String[] args)
     {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter Your Nickname: ");
+        String nickName = scanner.nextLine();
+
 
     }
 }
