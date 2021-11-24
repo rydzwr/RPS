90783aaafece5fe73982eaa563d1c8b8d3d8e2e2
rydzwr <rydzwr@gmail.com>
2021-11-24 02:11:26 +0100

    Created Project

---
+++ src/main/java/com/kodilla/rps_game/RpsGameApplication.java
@@ -0,0 +1,9 @@
+package com.kodilla.rps_game;
+
+public class RpsGameApplication
+{
+    public static void main(String[] args)
+    {
+
+    }
+}
---
+++ src/test/java/com/kodilla/rps_game/RpsGameApplicationTests.java
@@ -0,0 +1,15 @@
+package com.kodilla.rps_game;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RpsGameApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
